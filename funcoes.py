e7282fb3a1a7b1e9ef96355bbfc1d0b67c201783
Lucas Moreira <lucasdesm@gmail.com>
2025-04-28 14:50:54 -0300

    Função 1

--- funcoes.py
+++ funcoes.py
@@ -2,7 +2,7 @@ from random import *
 
 def rolar_dados(n):
     lista = []
-    for i in range(len(lista)):
-        n = randint(1,6)
-        lista.append(n)
+    for i in range(len(n)):
+        numeros = randint(1,6)
+        lista.append(numeros)
     return lista
