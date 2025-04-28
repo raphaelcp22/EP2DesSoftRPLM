21d78e01d2964bf9ca650ed325a9c6ef4ffb9964
Lucas Moreira <lucasdesm@gmail.com>
2025-04-28 15:21:48 -0300

    Função 2

--- funcoes.py
+++ funcoes.py
@@ -8,3 +8,13 @@ def rolar_dados(rodadas):
         lista.append(numeros)
 
     return lista
+
+def guardar_dado (lista1, lista2, numero):
+    dados_rolados = lista1
+    dados_no_estoque = lista2
+    indice = numero
+
+    dados_no_estoque.append(dados_rolados[indice])
+    
+    return [dados_rolados, dados_no_estoque]
+
