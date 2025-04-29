b7e36dd4605501d85926bb66c7c3c39c8eea3424
Lucas Moreira <lucasdesm@gmail.com>
2025-04-28 22:34:40 -0300

    Cacula pontos na regra avançada - Sequência baixa

--- funcoes.py
+++ funcoes.py
@@ -38,14 +38,14 @@ def calcula_pontos_soma (lista):
     return soma 
 
 def calcula_pontos_sequencia_baixa (lista_inteiros):
-    contador = 1
+    contador = 0
     pontuacao = 0
     lista_crescente = sorted(set(lista_inteiros))
     i = 1
     while i < len(lista_crescente):
         if lista_crescente[i] > lista_crescente[i-1]:
             contador += 1
-            if contador == 4:
+            if contador >= 3:
                 pontuacao += 15
         else: 
             contador = 1
