e43b5d49d3e682cde37e269c9b6c5850b77947a7
Raphael Primo <raphaelcp@al.insper.edu.br>
2025-05-06 19:55:12 -0300

    Question 13: Jogo - programa.py 2

--- programa.py
+++ programa.py
@@ -1,104 +1,115 @@
-# arquivo de programa
 from funcoes import *
-import random
 
-CATEGORIAS = [
-    "1", "2", "3", "4", "5", "6",
-    "sem_combinacao", "quadra", "full_house",
-    "sequencia_baixa", "sequencia_alta", "cinco_iguais"
-]
+numero_rodadas = 0
 
-cartela = {categoria: None for categoria in CATEGORIAS}
+tabela_pontos = {
+    'pontuacao_simples': {
+        1: -1,
+        2: -1,
+        3: -1,
+        4: -1,
+        5: -1,
+        6: -1
+    },
+    'pontuacao_avancada': {
+        'sem_combinacao': -1,
+        'quadra': -1,
+        'full_house': -1,
+        'sequencia_baixa': -1,
+        'sequencia_alta': -1,
+        'cinco_iguais': -1
+    }
+}
 
-def mostrar_dados(dados_rolados, dados_guardados):
-    print(f"Dados rolados: {dados_rolados}")
-    print(f"Dados guardados: {dados_guardados}")
+dados_atuais = rolar_dados(5)
+dados_guardados = []
 
-def jogada_valida(combinacao, cartela):
-    if combinacao not in cartela:
-        print("Combinação inválida. Tente novamente.")
-        return False
-    elif cartela[combinacao] is not None:
-        print("Essa combinação já foi utilizada.")
-        return False
-    return True
-
-def calcular_pontuacao(combinacao, dados):
-    if combinacao in ["1", "2", "3", "4", "5", "6"]:
-        return pontuacao_simples(dados, int(combinacao))
-    elif combinacao == "sem_combinacao":
-        return sum(dados)
-    elif combinacao == "quadra":
-        return pontuacao_quadra(dados)
-    elif combinacao == "full_house":
-        return pontuacao_full_house(dados)
-    elif combinacao == "sequencia_baixa":
-        return pontuacao_sequencia_baixa(dados)
-    elif combinacao == "sequencia_alta":
-        return pontuacao_sequencia_alta(dados)
-    elif combinacao == "cinco_iguais":
-        return pontuacao_cinco_iguais(dados)
-    return 0
-
-
-for _ in range(12):
-    dados_guardados = []
-    dados_rolados = [random.randint(1, 6) for _ in range(5)]
-    rerrolagens = 0
-
-    while True:
-        mostrar_dados(dados_rolados, dados_guardados)
-        print("Digite 1 para guardar um dado, 2 para remover um dado, 3 para rerrolar, 4 para ver a cartela ou 0 para marcar a pontuação:")
-        acao = input(">")
-
-        if acao == "1":
-            print("Digite o índice do dado a ser guardado (0 a 4):")
-            try:
-                i = int(input(">"))
-                if 0 <= i < len(dados_rolados):
-                    dados_guardados.append(dados_rolados.pop(i))
-                else:
-                    print("Índice inválido.")
-            except:
-                print("Entrada inválida.")
-
-        elif acao == "2":
-            print("Digite o índice do dado a ser removido (0 a 4):")
-            try:
-                i = int(input(">"))
-                if 0 <= i < len(dados_guardados):
-                    dados_rolados.append(dados_guardados.pop(i))
-                else:
-                    print("Índice inválido.")
-            except:
-                print("Entrada inválida.")
-
-        elif acao == "3":
-            if rerrolagens >= 2:
-                print("Você já usou todas as rerrolagens.")
+def jogar_rodada(tabela_pontos, dados_atuais, dados_guardados):
+    rerrolagens_feitas = 0
+    print('Digite 1 para guardar um dado, 2 para remover um dado, 3 para rerrolar, 4 para ver a cartela ou 0 para marcar a pontuação:')
+    acao = input('>')
+    while acao != '0':
+        if acao == '1':
+            print('Digite o índice do dado a ser guardado (0 a 4):')
+            indice = int(input('>'))
+            lista_dados = guardar_dado(dados_atuais, dados_guardados, indice)
+            dados_atuais = lista_dados[0]
+            dados_guardados = lista_dados[1]
+            print(f'Dados rolados: {dados_atuais}')
+            print(f'Dados guardados: {dados_guardados}')
+        elif acao == '2':
+            print('Digite o índice do dado a ser removido (0 a 4):')
+            indice = int(input('>'))
+            lista_dados = remover_dado(dados_atuais, dados_guardados, indice)
+            dados_atuais = lista_dados[0]
+            dados_guardados = lista_dados[1]
+            print(f'Dados rolados: {dados_atuais}')
+            print(f'Dados guardados: {dados_guardados}')
+        elif acao == '3':
+            if rerrolagens_feitas == 2:
+                print('Você já usou todas as rerrolagens.')
             else:
-                dados_rolados = [random.randint(1, 6) for _ in dados_rolados]
-                rerrolagens += 1
-
-        elif acao == "4":
-            imprime_cartela(cartela)
-
-        elif acao == "0":
-            print("Digite a combinação desejada:")
-            combinacao = input(">").strip()
-            if jogada_valida(combinacao, cartela):
-                todos_dados = dados_guardados + dados_rolados
-                cartela[combinacao] = calcular_pontuacao(combinacao, todos_dados)
-                break
+                dados_atuais = rolar_dados(len(dados_atuais))
+                rerrolagens_feitas += 1
+            print(f'Dados rolados: {dados_atuais}')
+            print(f'Dados guardados: {dados_guardados}')
+        elif acao == '4':
+            imprime_cartela(tabela_pontos)
+            print(f'Dados rolados: {dados_atuais}')
+            print(f'Dados guardados: {dados_guardados}')
         else:
             print("Opção inválida. Tente novamente.")
+        print('Digite 1 para guardar um dado, 2 para remover um dado, 3 para rerrolar, 4 para ver a cartela ou 0 para marcar a pontuação:')
+        acao = input('>')
 
+    dados_finais = dados_atuais + dados_guardados
+    print('Digite a combinação desejada:')
+    escolha_combinacao = input('>')
+    opcoes_numericas = ['1', '2', '3', '4', '5', '6']
+    while True:
+        if escolha_combinacao in opcoes_numericas:
+            categoria = int(escolha_combinacao)
+        else:
+            categoria = escolha_combinacao
 
-simples = ["1", "2", "3", "4", "5", "6"]
-pontos_simples = sum(cartela[c] for c in simples if cartela[c] is not None)
-bonus = 35 if pontos_simples >= 63 else 0
-pontuacao_total = sum(p for p in cartela.values() if p is not None) + bonus
+        if (categoria in tabela_pontos['pontuacao_simples'] and tabela_pontos['pontuacao_simples'][categoria] != -1) or \
+           (categoria in tabela_pontos['pontuacao_avancada'] and tabela_pontos['pontuacao_avancada'][categoria] != -1):
+            print("Essa combinação já foi utilizada.")
+            escolha_combinacao = input('> ')
+        elif categoria not in tabela_pontos['pontuacao_simples'] and categoria not in tabela_pontos['pontuacao_avancada']:
+            print("Combinação inválida. Tente novamente.")
+            escolha_combinacao = input('> ')
+        else:
+            break
 
+    faz_jogada(dados_finais, categoria, tabela_pontos)
+    return tabela_pontos
 
-imprime_cartela(cartela)
-print(f"Pontuação total: {pontuacao_total}")
+contador_rodadas = 0
+imprime_cartela(tabela_pontos)
+print(f'Dados rolados: {dados_atuais}')
+print(f'Dados guardados: {dados_guardados}')
+
+while contador_rodadas < 12:
+    tabela_pontos = jogar_rodada(tabela_pontos, dados_atuais, dados_guardados)
+    dados_atuais = rolar_dados(5)
+    dados_guardados = []
+    if contador_rodadas != 11:
+        print(f'Dados rolados: {dados_atuais}')
+        print(f'Dados guardados: {dados_guardados}')
+    contador_rodadas += 1
+
+subtotal_simples = 0
+pontuacao_total = 0
+
+for tipo_regra, valores in tabela_pontos.items():
+    for pontuacao in valores.values():
+        pontuacao_total += pontuacao
+        if tipo_regra == 'pontuacao_simples':
+            subtotal_simples += pontuacao
+
+if subtotal_simples >= 63:
+    pontuacao_total += 35
+
+imprime_cartela(tabela_pontos)
+print(f'Pontuação total: {pontuacao_total}')
